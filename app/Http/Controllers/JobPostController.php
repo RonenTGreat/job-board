db92d9cc2497d084e5b34247986169f9f3c3908a
RonenTGreat <ronenh53@gmail.com>
2025-12-27 14:26:41 +0000

    added max and min salary filter

--- app/Http/Controllers/JobPostController.php
+++ app/Http/Controllers/JobPostController.php
@@ -16,9 +16,14 @@ class JobPostController extends Controller
         $jobs = JobPost::query();
 
         $jobs->when(request('search'), function ($query) {
-            $query->where('title', 'like', '%' . request('search') . '%')
-                ->orWhere('description', 'like', '%' . request('search') . '%')
-            ;
+            $query->where(function ($query) {
+                $query->where('title', 'like', '%' . request('search') . '%')
+                ->orWhere('description', 'like', '%' . request('search') . '%');
+            });
+        })->when(request('min_salary'), function($query){
+            $query->where('salary', '>=', request('min_salary'));
+        })->when(request('max_salary'), function($query){
+            $query->where('salary', '<=', request('max_salary'));
         });
 
         return view('job.index', ['jobs' => $jobs->get()]);
