7da1ea296c37b758522ca330e5fd2e9f3d72c985
RonenTGreat <ronenh53@gmail.com>
2026-01-06 11:44:52 +0000

    Employer registration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\Employer;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use App\Models\JobPost;
+use App\Policies\EmployerPolicy;
 use App\Policies\JobPolicy;
 
 class AppServiceProvider extends ServiceProvider
@@ -24,5 +26,6 @@ class AppServiceProvider extends ServiceProvider
     {
         // Register the policy
         Gate::policy(JobPost::class, JobPolicy::class);
+        Gate::policy( Employer::class, EmployerPolicy::class);
     }
 }
